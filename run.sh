89bfce2f7504e3e331fd5d29330d982c263f0ea8
nsa ae <164992964+nsaae@users.noreply.github.com>
2024-03-26 13:36:01 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-f44a32ef-58b8-4f5d-878f-04cc29e1e79c/)
 
 
 echo "$curl_output" >> Update.tsx
